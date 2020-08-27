83764b200d71af874b4aa8087bfec9b2e986f3e9
Olaf Matyja <olafmat@gmail.com>
2020-08-27 04:30:15 +0200

    3414.96 2.92

--- 7.cpp
+++ 7.cpp
@@ -1365,7 +1365,7 @@ Game* compare(Board *board) {
         if (board->w < 25) {
             strategies.push_back(new MultiByAreaWithTabu<false, 14>(1));
             strategies.push_back(new MultiByAreaWithTabu<true, 5>(2));
-            strategies.push_back(new MultiByAreaWithTabu<true, 3>(3));
+            strategies.push_back(new MultiByAreaWithTabu<true, 2>(3));
             strategies.push_back(new MultiByAreaWithTabu<true, 3>(1));
             strategies.push_back(new MultiByAreaWithTabu<true, 3>(2));
             strategies.push_back(new MultiByAreaWithTabu<true, 2>(1));
