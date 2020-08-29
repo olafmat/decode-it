2a8a1fab5d452e8417ad88b2ff4f169ae5e1cf5d
Olaf Matyja <olafmat@gmail.com>
2020-08-29 18:33:18 +0200

    3455.73

--- 7.cpp
+++ 7.cpp
@@ -1313,10 +1313,10 @@ const Game* compare(Board *const board) {
     if (!board->colorHistogram[6]) {
         if (board->w < 25) {
             strategies.push_back(new MultiByAreaWithTabu<false, 14>(1));
-            strategies.push_back(new MultiByAreaWithTabu<true, 5>(2));
+            strategies.push_back(new MultiByAreaWithTabu<true, 4>(2));
             strategies.push_back(new MultiByAreaWithTabu<true, 2>(3));
             strategies.push_back(new MultiByAreaWithTabu<true, 2>(1));
-            strategies.push_back(new MultiByAreaWithTabu<true, 3>(2));
+            strategies.push_back(new EmptySlot());
             strategies.push_back(new EmptySlot());
             strategies.push_back(new MultiByAreaWithTabu<true, 2>(2));
             strategies.push_back(new MultiByAreaWithTabu<true, 3>(1));
