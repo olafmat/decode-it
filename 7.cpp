8f8748313539bc989d8356d6e3fc86ec072241e4
Olaf Matyja <olafmat@gmail.com>
2020-08-31 18:42:22 +0200

    3517.38 2.84

--- 7.cpp
+++ 7.cpp
@@ -1428,7 +1428,7 @@ const Game* compare(Board *const board) noexcept {
     } else if (!board->colorHistogram[15]) {
         M(24,1)
     } else if (!board->colorHistogram[16]) {
-        M(24,1)
+        M(23,1)
     } else {
         if (board->w < 25) {
             M(21,0) M(3,1) M(2,0)
